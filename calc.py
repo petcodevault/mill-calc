98b27b6f1265084da2d1c9ec24e19758b4234c56
Dmytro Kukushkin <codecity@gmail.com>
2025-11-16 11:03:17 +0200

    fix (classic)

--- calc.py
+++ calc.py
@@ -72,7 +72,7 @@ def has_valid_mill(player, color):
 # Classic
 neighbors = {
     A1: [A2, B1, B2],
-    A2: [A1, A3, B3],
+    A2: [A1, A3, B2],
     A3: [A2, B2, B3],
     B1: [A1, B2, C1],
     B2: [A1, A2, A3, B1, B3, C1, C2, C3],
